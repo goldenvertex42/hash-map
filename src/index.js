998c0a5a9b0c491a5ebe1947846627b93dfb7021
goldenvertex42 <fabled93@gmail.com>
2025-12-05 19:52:53 -0600

    Testing code for hash map and hash set

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
 import "./styles.css";
+import { HashMap } from "./hash-map";
+import { HashSet } from "./hash-set";
 
-console.log("This is goldenvertex's default template");
+const testMap = new HashMap;
+const testSet = new HashSet;
+
+
+testMap.set('apple', 'red')
+testMap.set('banana', 'yellow')
+testMap.set('carrot', 'orange')
+testMap.set('dog', 'brown')
+testMap.set('elephant', 'gray')
+testMap.set('frog', 'green')
+testMap.set('grape', 'purple')
+testMap.set('hat', 'black')
+testMap.set('ice cream', 'white')
+testMap.set('jacket', 'blue')
+testMap.set('kite', 'pink')
+testMap.set('lion', 'golden')
+testMap.set('oliver', 'goldenrod')
+
+
+testSet.set('apple');
+testSet.set('banana');
+testSet.set('orange');
+testSet.set('kiwi');
+testSet.set('strawberry');
+testSet.set('blueberry');
+testSet.set('pear');
+testSet.set('grape');
+testSet.set('plum');
+testSet.set('passionfruit');
+testSet.set('mango');
+testSet.set('pineapple');
+
+console.log(testSet.entries());
+
+
+
